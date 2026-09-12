f66f988094639bee056d90f4b8a11f067a8fd4a5
Dreamlexxi <guc39123@gmail.com>
2026-09-12 22:23:08 +0200

    Rename Fiks

--- server.lua
+++ server.lua
@@ -8,7 +8,7 @@ CreateThread(function()
         return
     end
 
-    PerformHttpRequest("https://raw.githubusercontent.com/Dreamlexxi/Sundhed-LB-Phone/main/version", function(status, latestVersion)
+    PerformHttpRequest("https://raw.githubusercontent.com/Dreamlexxi/lx_sundhed/main/version", function(status, latestVersion)
         if status ~= 200 or not latestVersion then
             return
         end
@@ -18,7 +18,7 @@ CreateThread(function()
         if latestVersion > currentVersion then
             print(("^3--------------------------------------------------^7"))
             print((string.format("^3[%s]^7 En ny version er tilgængelig: ^2v%s^7 (Nuværende: ^1v%s^7)", resource, latestVersion, currentVersion)))
-            print(("^3Hent den nyeste opdatering: ^4https://github.com/Dreamlexxi/Sundhed-LB-Phone^7"))
+            print(("^3Hent den nyeste opdatering: ^4https://github.com/Dreamlexxi/lx_sundhed^7"))
             print(("^3--------------------------------------------------^7"))
         end
     end, "GET")
